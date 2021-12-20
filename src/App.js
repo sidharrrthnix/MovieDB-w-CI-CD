cdf60309605c9eb0a78caec3524cddf1e7faa645
sidharrth <sidharrthnix@gmail.com>
2021-12-20 17:38:09 +0000

    header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import store from './redux/store';
+import './App.scss';
+import Header from './components/header';
 function App() {
-  return <Provider store={store}>Redux</Provider>;
+  return (
+    <Provider store={store}>
+      <Header />
+      Redux
+    </Provider>
+  );
 }
 
 export default App;
