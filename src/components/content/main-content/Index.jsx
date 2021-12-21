6f9acfe327a57f2fa4ddddda6eb2c97858952790
sidharrth <sidharrthnix@gmail.com>
2021-12-21 14:47:47 +0000

    paginate

--- src/components/content/main-content/Index.jsx
+++ src/components/content/main-content/Index.jsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import { useState } from 'react';
+import Paginate from '../paginate/Paginate';
 import Slideshow from '../slide-show/Slideshow';
 import './MainContent.scss';
 const MainContent = () => {
@@ -13,13 +15,26 @@ const MainContent = () => {
       url: 'https://w0.peakpx.com/wallpaper/720/8/HD-wallpaper-vijay-master-movie-poster.jpg'
     }
   ];
-
+  const [currentPage, setCurrentPage] = useState(1);
+  const paginate = (type) => {
+    if (type === 'prev' && currentPage > 1) {
+      setCurrentPage((prev) => prev - 1);
+    } else {
+      setCurrentPage((prev) => prev + 1);
+    }
+  };
   return (
     <div className="main-content">
       <Slideshow images={images} auto={false} />
       <div className="grid-movie-title">
         <div className="movieType">Now Playing</div>
-        <div className="paginate">Paginate</div>
+        <div className="paginate">
+          <Paginate
+            currentPage={currentPage}
+            totalPages={10}
+            paginate={paginate}
+          />
+        </div>
       </div>
     </div>
   );
